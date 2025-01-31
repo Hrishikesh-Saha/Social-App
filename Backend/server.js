ffbf0ed302e4b405025c41844e984174bb062312
Hrishi417 <powerofgamerz75@gmail.com>
2025-01-31 20:32:27 +0600

    10 - Ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
@@ -20,16 +21,24 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
-app.use(express.json({"limit" : "5mb"}));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification", notificationsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 app.listen({ port }, () => {
   console.log(`The server is listening to port ${port}....`);
